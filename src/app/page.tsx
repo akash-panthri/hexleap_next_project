c322569fab2fccfbf3d3b924eaa8e8da7a1e0c88
akash-panthri <akashpanthri2000@gmail.com>
2024-03-25 20:15:46 +0530

    changed a few styles in SpotlightCollectionContainer.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,11 @@
 import SpotlightCollectionContainer from "@/components/spotlightCollection/SpotlightCollectionContainer";
 import TeamElementContainer from "@/components/teamElementComponents/TeamElementContainer";
-import Image from "next/image";
-import Head from 'next/head';
+
 
 export default function Home() {
   return (
     <>
-     <Head>
-        <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;700&family=Inter:wght@400;700&display=swap" />
-      </Head>
+    
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
      <div className="">
       <TeamElementContainer />
--- src/components/spotlightCollection/SpotlightCollectionContainer.tsx
+++ src/components/spotlightCollection/SpotlightCollectionContainer.tsx
@@ -3,12 +3,12 @@ import SpotlightCollectionElementContainer from './SpotlightCollectionElementCon
 
 export default function SpotlightCollectionContainer() {
   return (
-    <div className='bg-[#f4f9ff]'>
-<div className="text-5xl font-bold font-poppins text-center">
+    <div className='bg-[#f4f9ff]  py-[7.5%]' >
+<div className="text-5xl font-bold font-poppins text-center my-[1.5%]">
 
 Collection Spotlight
 </div>
-<div className="text-sm font-inter font-normal">
+<div className="text-base font-inter font-normal text-center px-3 w-1/2 mx-[24%]  my-[1.5%]">
 
       Discover extraordinary moments with our Spotlight Collection metatickets—exclusive access to premium events for an unforgettable experience. Grab yours today!
 </div>
