95dbd068f34bbc88776975f01399e77f4c2e410c
akash-panthri <akashpanthri2000@gmail.com>
2024-03-28 15:25:49 +0530

    changed title of the page

--- src/components/teamElementComponents/TeamElementContainer.tsx
+++ src/components/teamElementComponents/TeamElementContainer.tsx
@@ -8,7 +8,7 @@ export default function TeamElementContainer() {
     <div className="xl:w-[1240px] bg-[#f7f7f8] dark:bg-[#080807] my-[1vh] flex flex-col py-[2.5%]">
 
      
-        <div className="border-b-2 border-blue-600 w-max mx-[11px] py-3 mb-7 text-3xl font-extrabold">
+        <div className="border-b-2 border-blue-600 w-max  py-3 mb-7 text-3xl font-extrabold">
           Sports
         </div>
      
