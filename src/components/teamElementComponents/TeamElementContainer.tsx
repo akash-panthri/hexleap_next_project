3806cc13257499aff03d33617ecb23d056ac3864
akash-panthri <akashpanthri2000@gmail.com>
2024-03-28 13:41:01 +0530

    fixed styling of 'sport' in TeamElement

--- src/components/teamElementComponents/TeamElementContainer.tsx
+++ src/components/teamElementComponents/TeamElementContainer.tsx
@@ -8,7 +8,7 @@ export default function TeamElementContainer() {
     <div className="w-[100%] bg-[#f7f7f8] dark:bg-[#080807]">
     <div className="mx-[2.5vw] my-[1vh] w-[93vw] flex flex-col py-[2.5%] px-[15%]">
      
-        <div className="border-b-2 border-blue-600 w-max mx-[calc(3rem_-_11px)] py-3 mb-7 text-3xl font-extrabold">
+        <div className="border-b-2 border-blue-600 w-max mx-[calc(1.5rem_-_11px)] py-3 mb-7 text-3xl font-extrabold">
           Sports
         </div>
      
